044e1f203dea5a06229ebe75a1afe9806cbaef3f
BhupeshRajpurohit <bhupesh.rajpurohit@crossasyst.com>
2022-05-29 22:13:21 +0530

    add react-icons in to tic-tac-toe project

---
+++ src/components/icon.jsx
@@ -0,0 +1,13 @@
+import { FaTimes, FaPen, FaRegCircle } from 'react-icons/fa';
+
+const Icon = ({ name }) => {
+  return (
+    <h1>
+      <FaTimes className="icon" />
+      <FaPen className="icon" />
+      <FaRegCircle className="icon" />
+    </h1>
+  );
+};
+
+export default Icon;
